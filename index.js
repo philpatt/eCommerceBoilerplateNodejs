3f29b82dd9efb15ff9e0194030516b28f9106a29
philpatt <phillip.s.patterson1@gmail.com>
2019-10-12 13:03:39 -0700

    begining db structure

--- index.js
+++ index.js
@@ -8,12 +8,22 @@ app.use(ejsLayouts)
 
 // Home Page
 app.get('/', function(req, res){
-    res.render('home.ejs')
+    res.render('home')
 });
 
 // About Page
 app.get('/about', function(req, res){
-    res.render('about/about.ejs')
+    res.render('about/about')
+})
+
+// Products Page
+app.get('/products', function (req, res) {
+    res.render('product/products')
+})
+
+// login/sign up Page
+app.get('/products', function (req, res) {
+    res.render('users/login')
 })
 
 app.listen(8000);
