324d2fff530d6fe21eef6551debd4d1f1be88321
philpatt <phillip.s.patterson1@gmail.com>
2019-10-12 16:41:09 -0700

    log in abillity created

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../models');
+var passport = require('../config/ppConfig');
 
 // Sign up
 router.get('/signup', function (req, res) {
@@ -21,23 +22,41 @@ router.post('/signup', function (req, res) {
     }).spread(function (user, created) {
         if (created) {
             // if created, success and redirect home
-            console.log('User created!');
-            res.redirect('/');
+            passport.authenticate('local', {
+                successRedirect: '/profile',
+                successFlash: 'Account created and logged in'
+            })(req, res);
         } else {
             // if not created, the email already exists
-            console.log('Email already exists');
+            // add error handling
+            req.flash('error', 'Email already exists');
             res.redirect('/auth/signup');
         }
     }).catch(function (error) {
         // if an error occurs, let's see what the error is
-        console.log('An error occurred: ', error.message);
+        // add error messaging
+        req.flash('error', error.message);
         res.redirect('/auth/signup');
     });
 });
 
-// Login page
+// Login
 router.get('/login', function (req, res) {
     res.render('auth/login');
+});
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/profile',
+    failureRedirect: '/auth/login',
+    failureFlash: 'Invalid username and/or password',
+    successFlash: 'You have logged in'
+}));
+
+// Logout
+router.get('/logout', function (req, res) {
+    req.logout();
+    req.flash('success', 'You have logged out');
+    res.redirect('/');
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -3,18 +3,37 @@ var express = require('express');
 var app = express();
 var ejsLayouts = require('express-ejs-layouts');
 var session = require('express-session');
+var passport = require('./config/ppConfig');
+var flash = require('connect-flash');
+var isLoggedIn = require('./middleware/isLoggedIn');
 
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true
-}));
 //set view engine
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 app.use(ejsLayouts)
+
+
+// initialize session
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+// flash notifications
+app.use(flash());
+
+// initialize the passport configuration and session as middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function (req, res, next) {
+    // before every route, attach the flash messages and current user to res.locals
+    res.locals.alerts = req.flash();
+    res.locals.currentUser = req.user;
+    next();
+});
 
 // Home Page
 app.get('/', function(req, res){
@@ -33,6 +52,14 @@ app.get('/products', function (req, res) {
 
 // controller routes
 app.use('/auth', require('./controllers/auth'));
+
+// product
+app.get('/profile', isLoggedIn, function(req, res){
+    res.render('profile');
+})
+// checkout
+// profile
+
 
 var server = app.listen(process.env.PORT || 8000);
 
