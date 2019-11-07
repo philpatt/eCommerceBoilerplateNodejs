5d9f215e4d3fe5c1c9fd1a84f1dc75b5121cce97
Phil Patterson <phil.patterson@brooksrunning.com>
2019-11-06 22:49:04 -0800

    updated logo

--- public/js/app.js
+++ public/js/app.js
@@ -3,11 +3,14 @@ console.log('app.js loaded..')
 // Nav UX/UI logic
 // load mobile nav
 
-const $burger = $('.burger')
-const $nav = $('.nav__list')
-const $navLinks = $('.nav__link')
+const $burger = $('.burger');
+const $nav = $('.nav__list');
+const $navLinks = $('.nav__link');
+let animateLinks = false;
 
 $burger.click(function(){
+    console.log('start', animateLinks)
+
     // toggle active
     $nav.toggleClass('nav__list--active');
 
@@ -15,14 +18,17 @@ $burger.click(function(){
     $burger.toggleClass('burger--active');
 
 
-    // fade in links
-    $navLinks.each((i, link) => {
-        if(link.style.animation) {
-            link.style.animation = '';
-        } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${i / 5 + 0.1}s`;
-        }
-    });
+    // link animation
+    // animateLinks = !animateLinks;
+    // $navLinks.each((i, link) => {
+    //     if(link.style.animation) {
+    //         link.style.animation = '';
+    //     } else {
+    //         link.style.animation = `navLinkFadeIn 0.5s ease forwards ${i / 5 + 0.1}s`;
+    //     }
+    // });
+
+
 
 
 });
